e656462bd85cf38d32ae6914e424dde5b2b86b39
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-12 11:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let s1 = s1.toLowerCase();
+    let s2 = s2.toLowerCase();
+	for(let i=0 ; i<s1.length ; i++){
+		let char = s1.charAt(i);
+		let x=0,y=i;
+		while(x <s2.length && y<s1.length && s1.charAt(x)==s2.charAt(y)){
+			x++;
+			y++;
+		}
+		if(x==s2.length){
+			return y - x;
+		}
+	}
+	return -1;
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
